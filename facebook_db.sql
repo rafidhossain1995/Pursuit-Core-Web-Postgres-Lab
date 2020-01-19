f04cfcf0048c5af793e6a78a0a12c46c1d731d58
rafidhossain1995 <rafidhossain@pursuit.org>
2020-01-19 15:13:14 -0500

    Insert Posts

--- facebook_db.sql
+++ facebook_db.sql
@@ -5,21 +5,21 @@ CREATE DATABASE facebook_db;
 
 \c facebook_db;
 
-DROP TABLE if EXISTS users;
-DROP TABLE if EXISTS posts;
-DROP TABLE if EXISTS likes;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS posts;
+DROP TABLE IF EXISTS likes;
 
 
 CREATE TABLE users(
     id SERIAL PRIMARY KEY,
-    name STRING,
+    name TEXT,
     age INTEGER
 );
 
 CREATE TABLE posts(
     id SERIAL PRIMARY KEY,
     poster_id INT REFERENCES users(id),
-    body STRING
+    body TEXT
 );
 
 CREATE TABLE likes(
@@ -28,6 +28,20 @@ CREATE TABLE likes(
     post_id INT REFERENCES posts(id)
 );
 
+INSERT INTO users(name, age)
+    VALUES('Rafid', 24),
+        ('Brandon', 23),
+        ('Corey', 29),
+        ('Dani', 28),
+        ('Maria', 35);
 
+INSERT INTO posts(body, poster_id)
+    VALUES('Good Morning Everyone', 1),
+        ('Getting Ready To Code Today', 2),
+        ('Hello World', 2),
+        ('Have A Blessed Morning', 2),
+        ('What is Everyone doing for lunch?', 3),
+        ('Getting Ready to code soon!', 4),
+        ('Pursuit is great', 5);
 
 
